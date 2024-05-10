e89c6047e8ed71545eb9d787cbcd8b2c3950caa6
teixeiragthiago <kbeca-thiago@hotmail.com>
2024-05-09 23:57:06 -0300

    feat: add dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// userController, err := di.SetupDependencies()
+	// if err != nil {
+	// 	log.Fatal("Error setting up dependencies", err)
+	// }
+
+	router := mux.NewRouter()
+
+	// routes.SetupRoutes(router, userController)
+
+	fmt.Print("Running API on port 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
