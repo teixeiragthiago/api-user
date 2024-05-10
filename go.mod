58e0b84deb6f8487296b4c7e67e0a8de09672b2f
teixeiragthiago <kbeca-thiago@hotmail.com>
2024-05-09 23:59:34 -0300

    feat: add dependencies

--- go.mod
+++ go.mod
@@ -5,7 +5,13 @@ go 1.22.2
 require gorm.io/gorm v1.25.10
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+)
+
+require (
 	github.com/gorilla/mux v1.8.1
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/driver/mysql v1.5.6
 )
--- internal/di/dependencies.go
+++ internal/di/dependencies.go
@@ -1,16 +1,33 @@
-// internal/di/dependencies.go
 package di
 
-// import (
-// 	"github.com/teixeiragthiago/api-user/internal/repository"
-// 	"github.com/teixeiragthiago/api-user/internal/service"
-// )
+import (
+	"github.com/teixeiragthiago/api-user/internal/controller"
+	"github.com/teixeiragthiago/api-user/internal/entity"
+	"github.com/teixeiragthiago/api-user/internal/repository"
+	"github.com/teixeiragthiago/api-user/internal/service"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
-// // func SetupDependencies() (*controller.UserController, error) {
-// // 	// Setup database connection, repositories, and services
-// // 	userRepo := repository.NewUserRepository(db)
-// // 	userService := service.NewUserService(userRepo)
-// // 	userController := controller.NewUserController(userService)
+// Setup dependencies
+func SetupDependencies() (*controller.UserController, error) {
+	// Setup database connection, repositories, and services
+	db, err := gorm.Open(mysql.Open("connString"), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
 
-// // 	return userController, nil
-// // }
+	//Migrate the schema
+	db.AutoMigrate(&entity.User{})
+
+	//Setup repository
+	userRepo := repository.NewUserRepository(db)
+
+	//Setup service
+	userService := service.NewUserService(userRepo)
+
+	//Setup controllers
+	userController := controller.NewUserController(userService)
+
+	return userController, nil
+}
