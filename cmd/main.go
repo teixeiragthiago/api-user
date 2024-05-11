0466d17daaff2dc3235f582a6308dce8df444638
teixeiragthiago <kbeca-thiago@hotmail.com>
2024-05-10 22:23:13 -0300

    feat: refact routes and DI files

--- cmd/main.go
+++ cmd/main.go
@@ -5,18 +5,12 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/teixeiragthiago/api-user/internal/di"
 )
 
 func main() {
-	// userController, err := di.SetupDependencies()
-	// if err != nil {
-	// 	log.Fatal("Error setting up dependencies", err)
-	// }
 
-	router := mux.NewRouter()
-
-	// routes.SetupRoutes(router, userController)
+	router := di.SetupControllers()
 
 	fmt.Print("Running API on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
