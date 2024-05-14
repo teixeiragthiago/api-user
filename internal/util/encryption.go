b823e9155ad5c1e6d939cfaa28514688e0b1f813
teixeiragthiago <kbeca-thiago@hotmail.com>
2024-05-13 21:39:09 -0300

    test: add test to encryption

--- internal/util/encryption.go
+++ internal/util/encryption.go
@@ -1,12 +1,18 @@
 package util
 
 import (
+	"errors"
 	"log"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 func EncryptPassword(password string) (string, error) {
+
+	if password == "" {
+		return "", errors.New("password cannot be empty")
+	}
+
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
 	if err != nil {
 		log.Fatal(err)
--- internal/util/encryption_test.go
+++ internal/util/encryption_test.go
@@ -1,1 +1,35 @@
 package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncryptPassword_ShouldEncryptWithSuccess(t *testing.T) {
+	//Arrange
+
+	assert := assert.New(t)
+
+	var plainPassword string = "123456789"
+
+	//Act
+	result, err := EncryptPassword(plainPassword)
+
+	//Assert
+	assert.NotEqual(plainPassword, result)
+	assert.Empty(err)
+}
+
+func TestEncryptPassword_ShouldReturnErrorWhenInputIsEmpty(t *testing.T) {
+	//Arrange
+	assert := assert.New(t)
+
+	//Act
+	result, err := EncryptPassword("")
+
+	//Assert
+	assert.Empty(result)
+	assert.Error(err)
+	assert.Equal("password cannot be empty", err.Error())
+}
