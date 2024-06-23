fbe8c2c6ccda153547c07587267055965757c3ca
teixeiragthiago <kbeca-thiago@hotmail.com>
2024-06-22 22:41:52 -0300

    test: add testes de unidade para o user_repository

--- internal/repository/user_repository_test.go
+++ internal/repository/user_repository_test.go
@@ -1,6 +1,7 @@
 package repository_test
 
 import (
+	"errors"
 	"regexp"
 	"testing"
 	"time"
@@ -9,28 +10,165 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/teixeiragthiago/api-user/internal/entity"
 	"github.com/teixeiragthiago/api-user/internal/repository"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	testutils "github.com/teixeiragthiago/api-user/internal/test_utils"
 )
+
+func TestUserRepository_InUserByAnotherUser_True(t *testing.T) {
+	//Arrange
+	gormDB, mock := testutils.SetupMockDB(t)
+	defer func() {
+		db, _ := gormDB.DB()
+		db.Close()
+	}()
+
+	userRepo := repository.NewUserRepository(gormDB)
+	userMock := mockValidUser()
+	rowsMock := mockRows(userMock)
+
+	userId := uint(1)
+	propParameter := "nickname"
+	input := "thiago_teste"
+
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `users` WHERE ? = LOWER(?) AND id != ? ORDER BY `users`.`id` LIMIT ?")).
+		WithArgs(propParameter, input, userId, 1).
+		WillReturnRows(rowsMock)
+
+	//Act
+	alreadyInUse, err := userRepo.InUseByAnotherUser(userId, propParameter, input)
+
+	//Assert
+	assert.NoError(t, err)
+
+	assert.Equal(t, true, alreadyInUse)
+
+	err = mock.ExpectationsWereMet()
+	assert.NoError(t, err)
+}
 
 func TestUserRepository_GetById_Success(t *testing.T) {
 
-	//arrange
-
-	db, mock, err := sqlmock.New()
-	assert.NoError(t, err)
-
-	defer db.Close()
-
-	mock.ExpectQuery(regexp.QuoteMeta("SELECT VERSION()")).WillReturnRows(sqlmock.NewRows([]string{"version"}).AddRow("1"))
-
-	gormDB, err := gorm.Open(mysql.New(mysql.Config{
-		Conn: db,
-	}), &gorm.Config{})
-	assert.NoError(t, err)
+	//Arrange
+	gormDB, mock := testutils.SetupMockDB(t)
+	defer func() {
+		db, _ := gormDB.DB()
+		db.Close()
+	}()
 
 	userRepo := repository.NewUserRepository(gormDB)
-	mockUser := &entity.User{
+	userMock := mockValidUser()
+	id := uint(1)
+
+	rows := mockRows(userMock)
+
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `users` WHERE id = ? ORDER BY `users`.`id` LIMIT ?")).
+		WithArgs(id, 1).
+		WillReturnRows(rows)
+
+	//Act
+	user, err := userRepo.GetById(id)
+
+	//Assert
+	assert.NoError(t, err)
+
+	assert.Equal(t, userMock, user)
+
+	err = mock.ExpectationsWereMet()
+	assert.NoError(t, err)
+}
+
+func TestUserRepository_GetById_Error(t *testing.T) {
+	//Arrange
+	gormDB, mock := testutils.SetupMockDB(t)
+	defer func() {
+		db, _ := gormDB.DB()
+		db.Close()
+	}()
+
+	userRepo := repository.NewUserRepository(gormDB)
+	id := uint(1)
+
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `users` WHERE id = ? ORDER BY `users`.`id` LIMIT ?")).
+		WithArgs(id, 1).
+		WillReturnError(errors.New("user not found"))
+
+	//Act
+	user, err := userRepo.GetById(id)
+
+	//Assert
+	assert.Error(t, err)
+	assert.Equal(t, "user not found", err.Error())
+	assert.Nil(t, user)
+
+	err = mock.ExpectationsWereMet()
+	assert.NoError(t, err)
+}
+
+func TestUserRepository_GetByEmail_Success(t *testing.T) {
+
+	//Arrange
+	gormDB, mock := testutils.SetupMockDB(t)
+	defer func() {
+		db, _ := gormDB.DB()
+		db.Close()
+	}()
+
+	userRepo := repository.NewUserRepository(gormDB)
+	userMock := mockValidUser()
+	email := "thiago@teste.com"
+
+	rows := mockRows(userMock)
+
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `users` WHERE email = ? ORDER BY `users`.`id` LIMIT ?")).
+		WithArgs(email, 1).
+		WillReturnRows(rows)
+
+	//Act
+	user, err := userRepo.GetByEmail(email)
+
+	//Assert
+	assert.NoError(t, err)
+
+	assert.Equal(t, userMock, user)
+
+	err = mock.ExpectationsWereMet()
+	assert.NoError(t, err)
+}
+
+func TestUserRepository_GetByEmail_Error(t *testing.T) {
+	//Arrange
+	gormDB, mock := testutils.SetupMockDB(t)
+	defer func() {
+		db, _ := gormDB.DB()
+		db.Close()
+	}()
+
+	userRepo := repository.NewUserRepository(gormDB)
+	email := "thiago@teste.com"
+
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `users` WHERE email = ? ORDER BY `users`.`id` LIMIT ?")).
+		WithArgs(email, 1).
+		WillReturnError(errors.New("user not found"))
+
+	//Act
+	user, err := userRepo.GetByEmail(email)
+
+	//Assert
+	assert.Error(t, err)
+	assert.Equal(t, "user not found", err.Error())
+	assert.Nil(t, user)
+
+	err = mock.ExpectationsWereMet()
+	assert.NoError(t, err)
+}
+
+func mockRows(mockUser *entity.User) *sqlmock.Rows {
+
+	return sqlmock.NewRows([]string{"id", "name", "email", "nickname", "password", "created_at", "active"}).
+		AddRow(mockUser.ID, mockUser.Name, mockUser.Email, mockUser.Nickname, mockUser.Password, mockUser.CreatedAt, mockUser.Active)
+}
+
+func mockValidUser() *entity.User {
+	return &entity.User{
 		ID:        1,
 		Name:      "Thiago",
 		Email:     "thiago@teste.com",
@@ -39,21 +177,4 @@ func TestUserRepository_GetById_Success(t *testing.T) {
 		CreatedAt: time.Now().Truncate(time.Millisecond),
 		Active:    true,
 	}
-	id := uint(1)
-
-	rows := sqlmock.NewRows([]string{"id", "name", "email", "nickname", "password", "created_at", "active"}).
-		AddRow(mockUser.ID, mockUser.Name, mockUser.Email, mockUser.Nickname, mockUser.Password, mockUser.CreatedAt, mockUser.Active)
-
-	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `users` WHERE id = ? ORDER BY `users`.`id` LIMIT ?")).
-		WithArgs(id, 1).
-		WillReturnRows(rows)
-
-	user, err := userRepo.GetById(id)
-
-	assert.NoError(t, err)
-
-	assert.Equal(t, mockUser, user)
-
-	err = mock.ExpectationsWereMet()
-	assert.NoError(t, err)
 }
---
+++ internal/test_utils/dbmock.go
@@ -0,0 +1,28 @@
+package testutils
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func SetupMockDB(t *testing.T) (*gorm.DB, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT VERSION()")).WillReturnRows(sqlmock.NewRows([]string{"version"}).AddRow("1"))
+
+	gormDB, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: db,
+	}), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	assert.NoError(t, err)
+
+	return gormDB, mock
+}
